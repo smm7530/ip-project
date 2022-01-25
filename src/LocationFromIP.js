4b3b1139c0bd68a1cf576f7fad1814c64782be41
Edwin Reyes <reyesedwin2022@gmail.com>
2022-01-25 13:17:42 -0500

    updated map in LocationFromIP

--- src/LocationFromIP.js
+++ src/LocationFromIP.js
@@ -9,14 +9,16 @@ export class LocationFromIP extends LitElement {
 
   constructor() {
     super();
-    this.UserIpInstance = new UserIP();
     this.locationEndpoint = 'https://freegeoip.app/json/';
-    this.long = 10.305385;
-    this.lat = 77.923029;
+    this.long = null;
+    this.lat = null;
   }
 
   static get properties() {
-    return {};
+    return {
+      long: { type: Number },
+      lat: { type: Number },
+    };
   }
 
   firstUpdated(changedProperties) {
@@ -38,6 +40,8 @@ export class LocationFromIP extends LitElement {
       })
       .then(data => {
         console.log(data);
+        this.long = data.longitude;
+        this.lat = data.latitude;
         return data;
       });
   }
@@ -59,7 +63,7 @@ export class LocationFromIP extends LitElement {
   render() {
     // this function runs every time a properties() declared variable changes
     // this means you can make new variables and then bind them this way if you like
-    const url = `https://maps.google.com/maps?q=${this.long},${this.lat}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
+    const url = `https://maps.google.com/maps?q=${this.lat},${this.long}&t=&z=15&ie=UTF8&iwloc=&output=embed`;
     return html`<iframe title="Where you are" src="${url}"></iframe> `;
   }
 }
